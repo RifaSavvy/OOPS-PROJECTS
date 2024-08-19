56be1371e00c52fb3433938a49b9501b222019fd
RifaSavvy <149214453+RifaSavvy@users.noreply.github.com>
2024-08-19 11:20:39 +0530

    Update Sumofn.java

--- TUTORIAL/Sumofn.java
+++ TUTORIAL/Sumofn.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
-
-class sumofnumbers {
+/*Rifa Basheer*/
+/*Roll no-51*/
+import java.util.*;
+class sumn {
     public static void main(String args[]) {
-        int sum=0;
-        int num, n;
-        
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the number of elements:");
-        n = s.nextInt();
-        
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            num = s.nextInt();
+        int s=0;
+        int num,n;
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter the no of elements:");
+        n = s1.nextInt();
+        System.out.println("Enter the string elements:");
+        for (int i = 0; i < n; i++){
+            num = s1.nextInt();
             {
-            	sum=sum+num;
+            	s=s+num;
             }
         }
-        
-        System.out.println("The sum of numbers is: " + sum);
-        s.close();
+        System.out.println("The sum of the numbers are: " + s);
+        s1.close();
     }
 }
