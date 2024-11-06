1e8755470f3f636b687ee8cc5595b652904ee4e6
RifaSavvy <149214453+RifaSavvy@users.noreply.github.com>
2024-11-06 15:16:56 +0530

    Add files via upload

--- swing.java
+++ swing.java
@@ -3,38 +3,49 @@ import java.awt.*;
 import java.awt.event.*;
 class test implements ActionListener
 {
-	JLabel l;
-	JTextField t;
-	JButton b;
-	test()
+JLabel l;
+JTextField t;
+JButton b;
+test()
+{
+	JFrame f=new JFrame("textfield");
+	l = new JLabel("nothing entered");
+	t = new JTextField(16);
+	b = new JButton("Submit");
+	JPanel p = new JPanel(new GridLayout(3,1));
+
+p.add(l);
+p.add(t);
+p.add(b);
+b.addActionListener(this);
+f.add(p);
+f.setSize(300,300);
+f.show();
+f.setTitle("Login");
+l.setText(t.getText());
+t.setText(" ");
+}
+
+public void actionPerformed(ActionEvent e)
+{
+	String s =e.getActionCommand();
+	if(s.equals("Submit"))
 	{
-		JFrame f=new JFrame("textfield");
-		l=new JLabel("nothing eneterd");
-		t=new JTextField(16);
-		b=new JButton("submit");
-		JPanel p=new JPanel(new GridLayout(1,3));
-		p.add(l);
-		p.add(t);
-		p.add(b);
-		b.addActionListener(this);
-		f.add(p);
-		f.setSize(300,300);
-		f.show();
-	}
-	public void actionPerformed(ActionEvent e)
-	{
-		String s=e.getActionCommand();
-		if(s.equals("submit"));
-		{
-			l.setText(t.getText());
-			t.setText(" ");
-		}
-	}
-	public static void main(String args[])
-	{
-		test t=new test();
+		l.setText(t.getText());
+		t.setText(" ");
 	}
 }
-			
-			
-		
+public static void main (String args[])
+	{
+ 	test t= new test();
+	}
+	}
+
+
+
+
+
+
+
+
+
